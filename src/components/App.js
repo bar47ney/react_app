4dc7c4325d81e9717159da0b86e7a6b47d2cadb1
bar47ney <bar47ney@gmail.com>
2022-02-17 21:08:59 +0300

    Create Users and ClassButtonComponents

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,50 @@
-import MyButton from "./MyButton/MyButton";
-import { useState } from "react";
-import ToggleButton from "./MyToggleButton/ToggleButton";
-import CounterHeader from "./Counter/CounterHeader";
-import CounterAdd from "./Counter/CounterAdd";
-import CounterSub from "./Counter/CounterSub";
-import CounterStudy from "./CounterStudy";
+// import MyButton from "./MyButton/MyButton";
+// import { useState } from "react";
+// import ToggleButton from "./MyToggleButton/ToggleButton";
+// import CounterHeader from "./Counter/CounterHeader";
+// import CounterAdd from "./Counter/CounterAdd";
+// import CounterSub from "./Counter/CounterSub";
+// import CounterStudy from "./CounterStudy";
+// import MyClassButton from "./MyClassButton/MyClassButton";
 
-function App() {
-  const [buttonText, setButtonText] = useState("Click me please!");
-  const [toggle, setToggle] = useState(false);
-  const [count, setCount] = useState(1);
+// function App() {
+//   const [buttonText, setButtonText] = useState("Click me please!");
+//   const [toggle, setToggle] = useState(false);
+//   const [count, setCount] = useState(1);
 
-  const changeText = () => {
-    setButtonText("New text");
-  };
+//   const changeText = () => {
+//     setButtonText("New text");
+//   };
 
+//   return (
+//     <div className="App">
+//       <MyButton action={changeText}>{buttonText}</MyButton>
+//       <ToggleButton toggle={toggle} setToggle={setToggle}>
+//         {toggle ? "One" : "Two"}
+//       </ToggleButton>
+//       <CounterHeader>Current count: {count}</CounterHeader>
+//       <hr></hr>
+//       <CounterAdd count={count} addCount={setCount}></CounterAdd>
+//       <CounterSub count={count} subCount={setCount}></CounterSub>
+//       <CounterStudy />
+//       <MyClassButton />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import UserList from "./UserList";
+import { users } from "../users";
+import UserAdd from "../UserAdd";
+
+const App = () => {
   return (
     <div className="App">
-      <MyButton action={changeText}>{buttonText}</MyButton>
-      <ToggleButton toggle={toggle} setToggle={setToggle}>
-        {toggle ? "One" : "Two"}
-      </ToggleButton>
-      <CounterHeader>Current count: {count}</CounterHeader>
-      <hr></hr>
-      <CounterAdd count={count} addCount={setCount}></CounterAdd>
-      <CounterSub count={count} subCount={setCount}></CounterSub>
-      <CounterStudy />
+      <UserList users={users} />
+      <UserAdd />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MyClassButton/MyClassButton.js
+++ src/components/MyClassButton/MyClassButton.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyClassButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.incrementClick = this.incrementClick.bind(this);
+  }
+  incrementClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
-    return <div></div>;
+    return <button onClick={this.incrementClick}>{this.state.count}</button>;
   }
 }
 
